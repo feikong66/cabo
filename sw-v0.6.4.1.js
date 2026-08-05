b348317ca045b621ff67d69695cf050dd5daa0a5
feikong66 <lcx20081120@outlook.com>
2026-08-05 21:24:28 +0800

    release 0.6.4: sw-v0.6.4.1.js

---
+++ sw-v0.6.4.1.js
@@ -0,0 +1,34 @@
+const CACHE = "cabo-v56";
+const FILES = [
+  "./",
+  "./CABO_v0.6.4.1.html",
+  "./manifest-641.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./assets/rules-4.jpg",
+  "./xlsx.full.min.js"
+];
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then((c) => Promise.all(FILES.map((f) => c.add(f).catch(() => null))))
+      .then(() => self.skipWaiting())
+  );
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).then((resp) => {
+      if (resp && resp.status === 200 && resp.type === "basic") {
+        const copy = resp.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+      }
+      return resp;
+    }).catch(() => r))
+  );
+});
